6e6617495befa81a74e7eb9cde23a84f3618538f
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-08-05 23:02:28 -0300

    Inicio do Row

---
+++ CodeWars/RowSumOddNumbers.java
@@ -0,0 +1,20 @@
+package CodeWars;
+
+public class RowSumOddNumbers {
+    public static void main(String[]args){
+
+    }
+
+    public static int rowSumOddNumbers(int n){
+        int[] linhas = new int[n];
+        int soma = 0;
+
+        for (int i = 0; i < linhas.length; i++) {
+            
+        }
+
+        return soma;
+    }
+
+    
+}
