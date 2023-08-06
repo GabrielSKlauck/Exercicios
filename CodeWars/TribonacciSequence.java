bf7a89b1e4e9d7d0d329e08ae12b3123e9298048
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2023-08-05 22:17:55 -0300

    Tribonacci feito

--- CodeWars/TribonacciSequence.java
+++ CodeWars/TribonacciSequence.java
@@ -1,17 +1,24 @@
 package CodeWars;
 
 public class TribonacciSequence {
+    //É tipo a sequencia de fibonacci, porem ao invez de usar 2 numeros usa-se 3
     public static void main(String[] args){
-        double[] lista = new double []{1,1,1};
-
-        System.out.println(percorre(tribonacci(lista, 10)));
+        double[] lista = new double []{0,1,1};
+        double[] lista2 = tribonacci(lista, 10);
+        System.out.println(percorre(lista2));
+        
+        
     }
 
     public static double[] tribonacci(double[] s, int n) {
         double[] sequence = new double[n];
+        try{
         sequence[0] = s[0];
         sequence[1] = s[1];
         sequence[2] = s[2];
+        }catch(IndexOutOfBoundsException e){
+            return sequence;
+        }
 
         for (int i = 0; i < sequence.length; i++) {
             try{
@@ -20,14 +27,14 @@ public class TribonacciSequence {
                 break;
             }
         }
-
+        
         return sequence;
     }
 
     public static String percorre(double[] s){
         String string = "";
         for (int i = 0; i < s.length; i++) {
-            string += s[0] + " ";
+            string += s[i] + " - ";
         }
         return string;
     }
